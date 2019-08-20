2c2566f046e7765ecba4e87332a9f6a9a3031c6f
x7Gv <sr3tgvsr2@gmail.com>
2019-08-20 03:13:30 +0300

    added some documentation

--- darray.h
+++ darray.h
@@ -13,14 +13,42 @@ typedef struct d_arr
 	size_t node_size;
 } d_arr;
 
+/**
+ *  Initialize d_arr to heap
+ *
+ *  @param arr       - ref pointer to associated d_array.
+ *  @param node_size - size of type to store in d_array 
+ */
 void arr_init(d_arr *arr, size_t node_size);
 
+/**
+ *  Append a value to the tail of array.
+ * 
+ *  @param arr  - ref pointer to associated d_array.
+ *  @param node - value to append
+ */
 void arr_append(d_arr *arr, void *node);
 
+/**
+ *  Get value from array by an index.
+ *
+ *  @param arr   - ref pointer to associated d_array.
+ *  @param index - index to array.
+ *  @param dest  - destination pointer for returned value.
+ */
 void arr_get(d_arr *arr, int index, void *dest);
 
-void arr_set(d_arr *arr, int index, void *dest);
+/**
+ *  Set value to the array by given index.
+ * 
+ *  @param arr  - ref pointer to associated d_array.
+ *  @param node - value to set.
+ */
+void arr_set(d_arr *arr, int index, void *node);
 
+/**
+ *  Free up used memory
+ */
 void arr_free(d_arr *arr);
 
 #endif
