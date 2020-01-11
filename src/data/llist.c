dc5fb7c0f1a4368fbcf4a7cb0832a362bd8f264c
x7Gv <sr3tgvsr2@gmail.com>
2020-01-11 04:10:16 +0200

    Added linked list functionality & documented llist header file

--- src/data/llist.c
+++ src/data/llist.c
@@ -5,3 +5,63 @@ void llist_init(llist *list, size_t member_size)
 	list->member_size = member_size;
 	list->head = NULL;
 }
+
+void llist_push(llist *list, void *data)
+{
+	node_t *new = (node_t *) malloc(list->member_size);
+	new->data = (char *) data;
+
+	llist_insert_begin(list, *(new));
+}
+
+void llist_append(llist *list, void *data)
+{
+	node_t *new = (node_t *) malloc(list->member_size);
+	node_t *last = list->head;
+
+	new->data = (char *) data;
+	new->next = NULL;
+
+	if (list->head == NULL) {
+		list->head = new;
+		return;
+	}
+
+	while (last->next != NULL) {
+		last = last->next;
+	}
+
+	llist_insert_after(*(last), *(new));
+
+	return;
+}
+
+void llist_insert_begin(llist *list, node_t new_node)
+{
+	new_node.next = list->head;
+	list->head = &new_node;
+}
+
+void llist_insert_after(node_t node, node_t new_node)
+{
+	new_node.next = node.next;
+	node.next = &new_node;
+}
+
+void llist_remove_after(node_t node)
+{
+	node_t obsolete = *((node_t *) node.next);
+	node.next = node.next->next;
+
+	free(obsolete.data);
+	free(&obsolete);
+}
+
+void llist_remove_begin(llist *list)
+{
+	node_t obsolete = *((node_t *) list->head);
+	list->head = list->head->next;
+
+	free(obsolete.data);
+	free(&obsolete);
+}
--- src/data/llist.h
+++ src/data/llist.h
@@ -3,23 +3,81 @@
 
 #include "stdlib.h"
 
-struct node
+/**
+ * @brief      Sub structure that contains the data to be linked.
+ */
+typedef struct node_t
 {
 	char *data;
-	struct node *next;
-};
+	node_t *next;
+}
+node_t;
 
-typedef struct llist 
+/**
+ * Main Link to the list.
+ */
+typedef struct llist_t
 {
-	struct node *head;
+	node_t *head;
 	size_t member_size;
-
-	unsigned int len;
 } 
 llist;
 
+/**
+ * @brief      Initialize the list 
+ *             [member_size]->NULL
+ * 
+ * @param      list         The list to initialize
+ * @param[in]  member_size  The member size
+ */
 void llist_init(llist *list, size_t member_size);
 
-void llist_add(llist *list, void *data);
+/**
+ * @brief      Append data to the tail of list.
+ *
+ * @param      list  The list to append
+ * @param      data  The data to append
+ */
+void llist_append(llist *list, void *data);
+
+/**
+ * @brief      Push data to the beginning of list.
+ *
+ * @param      list  The list to push to.
+ * @param      data  The data to push to.
+ */
+void llist_push(llist *list, void *data);
+
+/**
+ * @brief      Insert data to the beginning of list.
+ *
+ * @param      list      The list to insert to (head)
+ * @param[in]  new_node  The node to insert.
+ */
+void llist_insert_begin(llist *list, node_t new_node);
+
+/**
+ * @brief      Insert data after a given node.
+ *
+ * @param[in]  node      The node to insert to.
+ * @param[in]  new_node  The node to insert.
+ */
+void llist_insert_after(node_t node, node_t new_node);
+
+/**
+ * @brief      Remove node from the head of the list and
+ * 		link it again.
+ *
+ * @param      list  The list to do the operation.
+ */
+void llist_remove_begin(llist *list);
+
+/**
+ * @brief      Remove node from the tail of the list and
+ * 		link it again.
+ *
+ * @param      list  The list to do the operation.
+ */
+void llist_remove_after(node_t node);
 
 #endif
